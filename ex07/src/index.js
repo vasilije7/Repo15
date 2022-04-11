4e73cf78a82ca3cb638c2613db7b3b0f0ddd0a94
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-11 15:04:27 +0200

    Vjezba 7

---
+++ ex07/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+const sum = (num1, num2 = 1) => {
+  return num1 + num2;
+};
+console.log(sum(4, 6));
+console.log(sum(3));
+// Only change code above this line
+module.exports = sum;
