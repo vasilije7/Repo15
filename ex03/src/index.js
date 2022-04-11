b5ac8e8a34b0848e25281a1afe201b8f1af22466
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-11 14:44:47 +0200

    Izmjena vjezbe 3 i dodata vjezba 5

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,12 +1,12 @@
 function printManyTimes(str) {
   "use strict";
   // Only change code below this line
-  const sentence = str + " is cool!";
+  const SENTENCE = str + " is cool!";
   for (let i = 0; i < str.lenght; i += 2) {
-    console.log(sentence);
+    console.log(SENTENCE);
   }
-  return sentence;
+  return SENTENCE;
   // Only change code above this line
 }
-printManyTimes("Arena");
+console.log(printManyTimes("Arena"));
 module.exports = printManyTimes;
